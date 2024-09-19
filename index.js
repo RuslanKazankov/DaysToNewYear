2882df19ea1c2c5fcfb52a30acb7e259172f95c0
Ruslan Kazankov <130892261+RuslanKazankov@users.noreply.github.com>
2024-09-19 14:29:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const nextYear = new Date().getFullYear() + 1;
-const newYear = new Date(nextYear, 0, 1);
+let nextYear = new Date().getFullYear() + 1;
+let newYear = new Date(nextYear, 0, 1);
 
-const diffInMilliseconds = newYear - new Date();
+let diffInMilliseconds = newYear - new Date();
 
-const diffInDays = Math.ceil(diffInMilliseconds / (1000 * 60 * 60 * 24));
+let diffInDays = Math.ceil(diffInMilliseconds / (1000 * 60 * 60 * 24));
 
 console.log(diffInDays);
